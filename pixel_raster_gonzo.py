f57ea015832efbca07b0a78907f1a277de28637e
gonzalo-janer <90991639+gonzalo-janer@users.noreply.github.com>
2022-07-11 16:26:48 -0400

    Prototype working. x = 0 vertical is off.

--- pixel_raster_gonzo.py
+++ pixel_raster_gonzo.py
@@ -1,5 +1,6 @@
 # This script rasterizes a grey scale image into voxels with varying scale,
 # corresponding to the color value.
+from turtle import color
 import pymel.core as pm
 
 # RETURNS file texture object, for the shader of the selected object.
@@ -23,9 +24,15 @@ def get_voxel_scale(img_plane, x_res):
             length = abs(length)
             break
     voxel_scale = length / x_res
-    voxel_scale = [voxel_scale] * 3
-    return voxel_scale
+    voxel_scale_ls = [voxel_scale] * 3
+    return voxel_scale_ls
 
+def get_color_scale(voxel_scale_ls, rgb_value_ls):
+    color_scale_ls = []
+    for val in voxel_scale_ls:
+        color_scale_ls.append((1 - val) * rgb_value_ls[0])
+    return color_scale_ls
+        
 def main(x_resolution = None):
     img_plane = pm.selected()[0]
     file_node = get_file_texture(img_plane)
@@ -33,10 +40,11 @@ def main(x_resolution = None):
         resolution = file_node.outSize.get()
         x_res = int(resolution[0])
         y_res = int(resolution[1])
+    print(resolution)
     #TODO if x_resolution is different
-    parent_group = pm.group(name = 'parent_group', empty = True)
-    voxel_scale = get_voxel_scale(img_plane, x_res)
-    print(voxel_scale)
+    parent_group = pm.group(name = 'raster_parent_group', empty = True)
+    voxel_scale_ls = get_voxel_scale(img_plane, x_res)
+    print(voxel_scale_ls)
 
     for y in range(y_res):
         y += 0.5
@@ -45,10 +53,17 @@ def main(x_resolution = None):
             x += 0.5
             u_value = 1 / x_res * x
             world_position = pm.pointOnSurface(img_plane, u = u_value, v = v_value)
-            cube = pm.polyCube()[0]
-            cube.translate.set(world_position)
-            cube.scale.set(voxel_scale)
-            pm.parent(cube, parent_group)
+            rgb_value_ls = pm.colorAtPoint(file_node, output = 'RGB', u = u_value, v = v_value)
+            # invert color value
+            rgb_value_ls = [1 - val for val in rgb_value_ls]
+            color_scale_ls = get_color_scale(voxel_scale_ls, rgb_value_ls)
+            voxel_group = pm.group(name = 'voxel_group', empty = True)
+            voxel = pm.polyCube()[0]
+            pm.parent(voxel, voxel_group)
+            voxel_group.translate.set(world_position)
+            voxel_group.scale.set(voxel_scale_ls)
+            voxel.scale.set(rgb_value_ls)
+            pm.parent(voxel_group, parent_group)
 
 if __name__ == '__main__':
     main()
